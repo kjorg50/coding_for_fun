13b85cb1e0d9bda77e4d68ed4f6f5972723145c1
Kyle Jorgensen <kylewjorgensen@gmail.com>
2019-05-06 23:14:40 -0700

    Solve counting valleys problem

---
+++ counting_valleys/counting_valleys.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    valley_count = 0
+    current_height = 0
+
+    for letter in s:
+        if letter == 'D':
+            if current_height == 0:
+                valley_count += 1
+            current_height -= 1
+        elif letter == 'U':
+            current_height += 1
+
+    return valley_count
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    f = open("input", 'r')
+
+    n = int(f.readline())
+
+    s = f.readline()
+
+    result = countingValleys(n, s)
+
+    print(result)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
